8be7bc790a868949354ef10db48f47b7d1e8ac43
David Brainard <brainard@psych.upenn.edu>
2021-03-05 12:12:06 -0500

    Making slides to unpack ideas

--- code/fitEllipsesToIncrDecrData.m
+++ code/fitEllipsesToIncrDecrData.m
@@ -77,11 +77,11 @@ for n = 1:size(theData.paramsFitted_Multi,1)
 end
 
 %% Get incremet and decrement thresholds
-for ii = 1:length(theData.stimAngleList)       
-    if (theData.stimAngleList(ii) == 0)
-        incrThresh = abs(cosd(theData.stimAngleList(ii)).*modLevels_PF(ii,:));
-    elseif (theData.stimAngleList(ii) == 270)
-        decrThresh = abs(sind(theData.stimAngleList(ii)).*modLevels_PF(ii,:));
+for pp = 1:length(theData.stimAngleList)       
+    if (theData.stimAngleList(pp) == 0)
+        incrThresh = abs(cosd(theData.stimAngleList(pp)).*modLevels_PF(pp,:));
+    elseif (theData.stimAngleList(pp) == 270)
+        decrThresh = abs(sind(theData.stimAngleList(pp)).*modLevels_PF(pp,:));
     end      
 end
 
@@ -91,34 +91,40 @@ end
 % which makes the plots look better and also lets us
 % make some didactically useful plots.
 index = 1;
-for ii = 1:length(theData.stimAngleList)
+for pp = 1:length(theData.stimAngleList)
     % Get data in x,y form
-    xPlot_Fit(index,:) = cosd(theData.stimAngleList(ii)).*modLevels_PF(ii,:);
-    yPlot_Fit(index,:) = sind(theData.stimAngleList(ii)).*modLevels_PF(ii,:);
+    xPlot_Fit(index,:) = cosd(theData.stimAngleList(pp)).*modLevels_PF(pp,:);
+    yPlot_Fit(index,:) = sind(theData.stimAngleList(pp)).*modLevels_PF(pp,:);
+    angles_Fit(index) = theData.stimAngleList(pp);
     index = index+1;
     
     % This does the reflection.  Double up points on diagonal so that each
     % datum counts same number of times in the fit.
     if (reflect)
-        if (theData.stimAngleList(ii) == 0)
-            xPlot_Fit(index,:) = cosd(90).*modLevels_PF(ii,:);
-            yPlot_Fit(index,:) = sind(90).*modLevels_PF(ii,:);
+        if (theData.stimAngleList(pp) == 0)
+            xPlot_Fit(index,:) = cosd(90).*modLevels_PF(pp,:);
+            yPlot_Fit(index,:) = sind(90).*modLevels_PF(pp,:);
+            angles_Fit(index) = 90;
             index = index+1;
-        elseif (theData.stimAngleList(ii) == 270)
-            xPlot_Fit(index,:) = cosd(180).*modLevels_PF(ii,:);
-            yPlot_Fit(index,:) = sind(180).*modLevels_PF(ii,:);
+        elseif (theData.stimAngleList(pp) == 270)
+            xPlot_Fit(index,:) = cosd(180).*modLevels_PF(pp,:);
+            yPlot_Fit(index,:) = sind(180).*modLevels_PF(pp,:);
+            angles_Fit(index) = 180;  
             index = index+1;
-        elseif (theData.stimAngleList(ii) == 45)
-            xPlot_Fit(index,:) = cosd(45).*modLevels_PF(ii,:);
-            yPlot_Fit(index,:) = sind(45).*modLevels_PF(ii,:);
+        elseif (theData.stimAngleList(pp) == 45)
+            xPlot_Fit(index,:) = cosd(45).*modLevels_PF(pp,:);
+            yPlot_Fit(index,:) = sind(45).*modLevels_PF(pp,:);
+            angles_Fit(index) = 45;
             index = index+1;
-        elseif (theData.stimAngleList(ii) == 225)
-            xPlot_Fit(index,:) = cosd(225).*modLevels_PF(ii,:);
-            yPlot_Fit(index,:) = sind(225).*modLevels_PF(ii,:);
+        elseif (theData.stimAngleList(pp) == 225)
+            xPlot_Fit(index,:) = cosd(225).*modLevels_PF(pp,:);
+            yPlot_Fit(index,:) = sind(225).*modLevels_PF(pp,:);
+            angles_Fit(index) = 225;
             index = index+1;
-        elseif (theData.stimAngleList(ii) < 0)
-            xPlot_Fit(index,:) = cosd(90-theData.stimAngleList(ii)).*modLevels_PF(ii,:);
-            yPlot_Fit(index,:) = sind(90-theData.stimAngleList(ii)).*modLevels_PF(ii,:);
+        elseif (theData.stimAngleList(pp) < 0)
+            xPlot_Fit(index,:) = cosd(90-theData.stimAngleList(pp)).*modLevels_PF(pp,:);
+            yPlot_Fit(index,:) = sind(90-theData.stimAngleList(pp)).*modLevels_PF(pp,:);
+            angles_Fit(index) = 90-theData.stimAngleList(pp);
             index = index+1;
         end
     end
@@ -127,13 +133,13 @@ end
 %% Make increments and decrements same scale, if desired
 if (scaleIncrDecr)
     incrDecrScaleFactor = incrThresh./decrThresh;
-    for ii = 1:size(xPlot_Fit,1)
+    for pp = 1:size(xPlot_Fit,1)
         for jj = 1:size(xPlot_Fit,2)
-            if (xPlot_Fit(ii,jj) < 0)
-                xPlot_Fit(ii,jj) = incrDecrScaleFactor(jj)*xPlot_Fit(ii,jj);
+            if (xPlot_Fit(pp,jj) < 0)
+                xPlot_Fit(pp,jj) = incrDecrScaleFactor(jj)*xPlot_Fit(pp,jj);
             end
-            if (yPlot_Fit(ii,jj) < 0)
-                yPlot_Fit(ii,jj) = incrDecrScaleFactor(jj)*yPlot_Fit(ii,jj);
+            if (yPlot_Fit(pp,jj) < 0)
+                yPlot_Fit(pp,jj) = incrDecrScaleFactor(jj)*yPlot_Fit(pp,jj);
             end
         end
     end
@@ -148,11 +154,11 @@ end
 % propsSeen = [0.50 0.70 0.90];
 propsSeen = 0.70;
 if (length(propsSeen) > 1)
-    for ii = 1:length(propsSeen)
-        whichCol = find(propSeen_Fit == propsSeen(ii));
+    for pp = 1:length(propsSeen)
+        whichCol = find(propSeen_Fit == propsSeen(pp));
         xData = xPlot_Fit(:,whichCol)';
         yData = yPlot_Fit(:,whichCol)';
-        theDataToFit{ii} = [xData ; yData];
+        theDataToFit{pp} = [xData ; yData];
     end
 else
     whichCol = find(propSeen_Fit == propsSeen);
@@ -166,21 +172,14 @@ theColors = ['r' 'k' 'b' 'b' 'y' 'c'];
 if (length(propsSeen) > length(theColors))
     error('Need to specify more plot colors, fewer propsSeen, or fix code to handle');
 end
-theFig = figure; clf; hold on;
+theEllipseFig = figure; clf; hold on;
 if (iscell(theDataToFit))
-    for ii = 1:length(propsSeen)
-        plot(theDataToFit{ii}(1,:),theDataToFit{ii}(2,:),[theColors(ii) 'o'],'MarkerFaceColor',theColors(ii),'MarkerSize',12);
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,:),theDataToFit{pp}(2,:),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
     end
 else
     plot(theDataToFit(1,:),theDataToFit(2,:),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
 end
-
-theLim = 1;
-plot([-theLim theLim],[0 0],'k:','LineWidth',1);
-plot([0 0],[-theLim theLim],'k:','LineWidth',1);
-xlim([-theLim theLim]);
-ylim([-theLim theLim]);
-axis('square');
 
 %% Fit.
 [ellParams,A,Ainv,Q] = FitEllipseQ(theDataToFit,'lockAngleAt0',lockAngleAt0,'errorScalar',errorScalar,'initialParams',[incrThresh(end) decrThresh(end) 45]);
@@ -228,10 +227,125 @@ else
     plot(ellPoints(1,:),ellPoints(2,:),'r','LineWidth',2);
     title(sprintf('Subject %s, criterion %d%%',subj,round(100*propsSeen)));
 end
-xlabel('Stimulus 1')
-ylabel('Stimulus 2');
-print(gcf, fullfile(analysisDir,sprintf('%s_%d_%d_Ellipse.tiff', subj,normFlag,correctForGuessing)), '-dtiff');
+theLim = 0.5;
+plot([-theLim theLim],[0 0],'k:','LineWidth',1);
+plot([0 0],[-theLim theLim],'k:','LineWidth',1);
+xlim([-theLim theLim]);
+ylim([-theLim theLim]);
+axis('square');
+xlabel('Contrast 1')
+ylabel('Contrast 2');
+print(theEllipseFig, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_Ellipse.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
 
-%% Save fit data to mat file
-% save(fullfile(analysisDir,sprintf('%s_%d_incDecFits_ConstrainedSlope.mat', subj,normFlag)), 'stimAngleList', 'falsePosProp', 'paramsFitted_Individual', 'paramsFitted_Multi', 'PF');
-% print(gcf, fullfile(analysisDir,sprintf('%s_%d_incDecFits_ConstrainedSlope.png', subj,normFlag)), '-dpng2');
+%% Build up some explanatory plots
+%
+% Just incremental stimulus one
+theIncrFig = figure; clf; hold on
+index = find(angles_Fit == 0);
+if (iscell(theDataToFit))
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,index),theDataToFit{pp}(2,index),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
+    end
+else
+    plot(theDataToFit(1,index),theDataToFit(2,index),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
+end
+plot([-theLim theLim],[0 0],'k:','LineWidth',1);
+plot([0 0],[-theLim theLim],'k:','LineWidth',1);
+xlim([-theLim theLim]);
+ylim([-theLim theLim]);
+axis('square');
+xlabel('Contrast 1')
+ylabel('Contrast 2');
+print(theIncrFig, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_SingleIncr.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Both incremental stimuli
+index = find(angles_Fit == 90);
+if (iscell(theDataToFit))
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,index),theDataToFit{pp}(2,index),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
+    end
+else
+    plot(theDataToFit(1,index),theDataToFit(2,index),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
+end
+print(theIncrFig, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_DoubleIncr.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+theIncrFig1 = theIncrFig.copy;
+figure(theIncrFig);
+
+%% Fit linear model to single increments and plot
+neededAngles = [0 90];
+index = [];
+for aa = 1:length(neededAngles)
+    index = [index find(angles_Fit == neededAngles(aa))];
+end
+x = theDataToFit(1,index)';
+y = theDataToFit(2,index)';
+lineParams = [x ones(size(x))]\y;
+linePlotX = linspace(-theLim,theLim,100)';
+linePlotY = [linePlotX ones(size(linePlotX))]*lineParams;
+plot(linePlotX,linePlotY,'r','LineWidth',3);
+print(theIncrFig, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_DoubleIncrWithLine.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Add incr-incr datum
+if (iscell(theDataToFit))
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,index),theDataToFit{pp}(2,index),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
+    end
+else
+    plot(theDataToFit(1,index),theDataToFit(2,index),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
+end
+print(theIncrFig1, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_IncrWithDoubleIncrLine.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Add incr-incr
+figure(theIncrFig1);
+index = find(angles_Fit == 45);
+if (iscell(theDataToFit))
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,index),theDataToFit{pp}(2,index),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
+    end
+else
+    plot(theDataToFit(1,index),theDataToFit(2,index),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
+end
+print(theIncrFig1, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_Incr.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Fit linear model to single increments and plot
+neededAngles = [0 45 90];
+index = [];
+for aa = 1:length(neededAngles)
+    index = [index find(angles_Fit == neededAngles(aa))];
+end
+x = theDataToFit(1,index)';
+y = theDataToFit(2,index)';
+lineParams = [x ones(size(x))]\y;
+linePlotX = linspace(-theLim,theLim,100)';
+linePlotY = [linePlotX ones(size(linePlotX))]*lineParams;
+plot(linePlotX,linePlotY,'r','LineWidth',3);
+print(theIncrFig1, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_IncrWithBestIncrLine.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Add decr-decr points
+neededAngles = [180 225 270];
+index = [];
+for aa = 1:length(neededAngles)
+    index = [index find(angles_Fit == neededAngles(aa))];
+end
+if (iscell(theDataToFit))
+    for pp = 1:length(propsSeen)
+        plot(theDataToFit{pp}(1,index),theDataToFit{pp}(2,index),[theColors(pp) 'o'],'MarkerFaceColor',theColors(pp),'MarkerSize',12);
+    end
+else
+    plot(theDataToFit(1,index),theDataToFit(2,index),[theColors(1) 'o'],'MarkerFaceColor',theColors(1),'MarkerSize',12); 
+end
+print(theIncrFig1, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_IncrDecrWithBestIncrLine.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
+
+%% Add decr-decr line
+neededAngles = [180 225 270];
+index = [];
+for aa = 1:length(neededAngles)
+    index = [index find(angles_Fit == neededAngles(aa))];
+end
+x = theDataToFit(1,index)';
+y = theDataToFit(2,index)';
+lineParams = [x ones(size(x))]\y;
+linePlotX = linspace(-theLim,theLim,100)';
+linePlotY = [linePlotX ones(size(linePlotX))]*lineParams;
+plot(linePlotX,linePlotY,'r','LineWidth',3);
+print(theIncrFig1, fullfile(analysisDir,sprintf('%s_%d_%d_%d_%d_IncrDecrWithBestIncrDecrLines.tiff', subj,normFlag,correctForGuessing,lockAngleAt0,scaleIncrDecr)), '-dtiff');
