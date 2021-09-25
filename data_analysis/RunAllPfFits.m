a4e15837b82b0fcde9dc6e187d4e64c274f72055
David Brainard <brainard@psych.upenn.edu>
2021-09-25 17:18:39 -0400

    Add options for 2021-09-14 data

--- data_analysis/RunAllPfFits.m
+++ data_analysis/RunAllPfFits.m
@@ -11,3 +11,13 @@ fitPfsToIncrDecrData('subj','11043','dataDate','20200131','subProject','IncrDecr
     'norm',false','corrGuess',true,'refl',true);
 fitPfsToIncrDecrData('subj','11046','dataDate','20200131','subProject','IncrDecr1','condition','Separation_1', ...
     'norm',false','corrGuess',true,'refl',true);
+
+
+fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_1', ...
+    'norm',false','corrGuess',true,'refl',false);
+fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_1', ...
+    'norm',false','corrGuess',true,'refl',true);
+
+
+% fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_2', ...
+%     'norm',false','corrGuess',true,'refl',true);
