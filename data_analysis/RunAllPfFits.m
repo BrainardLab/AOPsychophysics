4200b5b80c22c1bb049abb8cc10617db6b891b50
David Brainard <brainard@psych.upenn.edu>
2021-10-25 08:22:46 -0400

    Commenting out analysis of unusable (too small, uncrossed stim design) data

--- data_analysis/RunAllPfFits.m
+++ data_analysis/RunAllPfFits.m
@@ -31,14 +31,19 @@ fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr
 
 % 5x7 pixel rectangular stimulus, 11046
 %
-% This was just one run and the stimulus levels were
-% duplicated, which means it is a mess.  Data not 
-% usable but motivated us to do it more carefully later
-% on.
-fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_2', ...
-    'norm',false','corrGuess',true,'refl',false);
-fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_2', ...
-    'norm',false','corrGuess',true,'refl',false);
+% This was just one run and the stimulus levels were duplicated, which
+% means it is a mess.  Data not usable but motivated us to do it more
+% carefully later on.
+%
+% To run this, you have to uncomment some kluges in fitPfsToIncrDecrData
+% which are currently commented out.  Those kluges massage the data a
+% little so it looks like there was a fully crossed stimulus design. Search
+% on string 9/14/21 to find the relevant code sections.
+%
+% fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_2', ...
+%     'norm',false','corrGuess',true,'refl',false);
+% fitPfsToIncrDecrData('subj','11046','dataDate','20210914','subProject','IncrDecr2','condition','Size_2', ...
+%     'norm',false','corrGuess',true,'refl',false);
 
 %% 2021-10-18
 %
@@ -50,10 +55,14 @@ fitPfsToIncrDecrData('subj','11046','dataDate','20211018','subProject','IncrDecr
 
 % 4x8 pixel rectangular stimulus, 11046
 % 
-% Just one run.  Don't see the incr-decr, and 
-% hardly see anything else.  Too small given
-% gamut limitations.
-fitPfsToIncrDecrData('subj','11046','dataDate','20211018','subProject','IncrDecr2','condition','Size_3', ...
-    'norm',false','corrGuess',true,'refl',true);
-fitPfsToIncrDecrData('subj','11046','dataDate','20211018','subProject','IncrDecr2','condition','Size_3', ...
-    'norm',false','corrGuess',true,'refl',false);
+% Just one run.  Don't see the incr-decr, and hardly see anything else.
+% Too small given gamut limitations.
+%
+% For some reason I haven't tracked down, the 'refl',false case crashes,
+% complaing about violation of the same number of levels in each direction.
+% I have not tracked that down.
+%
+% fitPfsToIncrDecrData('subj','11046','dataDate','20211018','subProject','IncrDecr2','condition','Size_3', ...
+%     'norm',false','corrGuess',true,'refl',true);
+% fitPfsToIncrDecrData('subj','11046','dataDate','20211018','subProject','IncrDecr2','condition','Size_3', ...
+%     'norm',false','corrGuess',true,'refl',false);
